05ea439fdbaf27a54a373c841a586fe7ec7323ae
OCANDABEAST <81494306+OCANDABEAST@users.noreply.github.com>
2021-10-28 21:00:01 -0400

    Add files via upload
    we worked on clown nose x y axis

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function preload() {   
-}
+noseX=0;
+noseY=0;
 
+
+function preload() {   
+  clown_nose = loadImage('https://i.postimg.cc/TPtp9GKZ/unnamed.png');
+}
 
 function setup() {
 canvas = createCanvas(300, 300);
@@ -23,6 +27,8 @@ function gotPoses(results)
  if(results.length > 0)
   {
      console.log(results);
+     noseX = results[0].pose.nose.x-15;
+     noseY = results[0].pose.nose.y-15;
      console.log("nose x = " + results[0].pose.nose.x);
      console.log("nose y = " + results[0].pose.nose.y);
   }
@@ -31,11 +37,13 @@ function gotPoses(results)
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
+    /*fill(255,0,0);
+    stroke(225,0,0);
+    circle(noseX, noseY, 20);*/
 }
 
 
 function take_snapshot(){
 save('myFilterImage.png');
 }
-
-
